78b35835870a4850be99d51ef7fa24bce110870e
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-01-25 14:19:35 -0500

    add exlixhauser baseline 3 years

--- iptw/main.py
+++ iptw/main.py
@@ -21,13 +21,13 @@ print = functools.partial(print, flush=True)
 def parse_args():
     parser = argparse.ArgumentParser(description='process parameters')
     # Input
-    parser.add_argument('--dataset', choices=['COL', 'MSHS', 'MONTE', 'NYU', 'WCM'], default='COL', help='site dataset')
+    parser.add_argument('--dataset', choices=['COL', 'MSHS', 'MONTE', 'NYU', 'WCM'], default='ALL', help='site dataset')
     parser.add_argument("--random_seed", type=int, default=0)
-    parser.add_argument('--run_model', choices=['LSTM', 'LR', 'MLP', 'XGBOOST', 'LIGHTGBM'], default='MLP')
+    # parser.add_argument('--run_model', choices=['LSTM', 'LR', 'MLP', 'XGBOOST', 'LIGHTGBM'], default='MLP')
     args = parser.parse_args()
 
     # More args
-    args.data_file = r'../data/V15_COVID19/output/cohorts_covariates_df_{}.csv'.format(args.dataset)
+    args.data_file = r'../data/V15_COVID19/output/character/pcr_cohorts_covariate_elixh_encoding_{}.csv'.format(args.dataset)
 
     if args.random_seed < 0:
         from datetime import datetime
@@ -65,13 +65,14 @@ if __name__ == "__main__":
 
     # %% 1. Load Data
     print('Load data file:', args.data_file)
-    df = pd.read_csv(args.data_file, dtype={'patid': str, 'covid': int})
+    df = pd.read_csv(args.data_file,
+                     dtype={'patid': str, 'covid': int})
     # .set_index('patid')
     # because a patid id may occur in multiple sites. patid wers site specific
 
-    df_info = df[['patid', 'site']]
+    df_info = df[['Unnamed: 0', 'patid', 'site']]
     df_label = df['covid']
-    df_covs = df.iloc[:, 3:]
+    df_covs = df.iloc[:, df.columns.get_loc('age20-39'):]
     print('df.shape:', df.shape)
     print('df_covs.shape:', df_covs.shape)
     # df_covs changed to 0 or 1 for the count columns
@@ -93,11 +94,11 @@ if __name__ == "__main__":
 
     df_covs_array = (df_covs_include > 0).astype('float')
 
-    model = ml.PropensityEstimator(learner='LR', paras_grid={'penalty': 'l2',
-                                                             'C': 0.03162277660168379,
-                                                             'max_iter': 200,
-                                                             'random_state': 0}).cross_validation_fit(df_covs_array, df_label)
-
+    model = ml.PropensityEstimator(learner='LR').cross_validation_fit(df_covs_array, df_label)
+    # , paras_grid = {'penalty': 'l2',
+    #                 'C': 0.03162277660168379,
+    #                 'max_iter': 200,
+    #                 'random_state': 0}
     ps = model.predict_ps(df_covs_array)
     iptw = model.predict_inverse_weight(df_covs_array, df_label, stabilized=True, clip=False)
     smd, smd_weighted = model.predict_smd(df_covs_array, df_label, abs=False, verbose=True)
@@ -105,6 +106,6 @@ if __name__ == "__main__":
     plt.plot(smd_weighted)
     plt.show()
 
-    model.results.to_csv('evaluation_ALL-model-select2.csv')  # args.save_model_filename +
+    model.results.to_csv('../data/V15_COVID19/output/character/evaluation_elixhauser_encoding_model_selection.csv')  # args.save_model_filename +
 
     print('Done! Total Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
