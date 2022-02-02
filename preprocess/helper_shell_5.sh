d757e16720293beb756b29bf08d316e073e56979
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-02-02 10:46:44 -0500

    use pcr + antigen, rerun  in bash

---
+++ preprocess/helper_shell_5.sh
@@ -0,0 +1,5 @@
+python pre_immun.py --dataset COL 2>&1 | tee  log/pre_immun_COL.txt
+python pre_immun.py --dataset WCM 2>&1 | tee  log/pre_immun_WCM.txt
+python pre_immun.py --dataset NYU 2>&1 | tee  log/pre_immun_NYU.txt
+python pre_immun.py --dataset MONTE 2>&1 | tee  log/pre_immun_MONTE.txt
+python pre_immun.py --dataset MSHS 2>&1 | tee  log/pre_immun_MSHS.txt
