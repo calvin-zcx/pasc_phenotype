4c08a392a2eaf6204837a03670a014d205b7857f
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-02-11 08:41:39 -0500

    pre cohort _incidence

---
+++ preprocess/helper_incidence.sh
@@ -0,0 +1,5 @@
+python pre_cohort_incidence.py --dataset COL 2>&1 | tee  log/pre_cohort_incidence_COL.txt
+python pre_cohort_incidence.py --dataset WCM 2>&1 | tee  log/pre_cohort_incidence_WCM.txt
+python pre_cohort_incidence.py --dataset NYU 2>&1 | tee  log/pre_cohort_incidence_NYU.txt
+python pre_cohort_incidence.py --dataset MONTE 2>&1 | tee  log/pre_cohort_incidence_MONTE.txt
+python pre_cohort_incidence.py --dataset MSHS 2>&1 | tee  log/pre_cohort_incidence_MSHS.txt
