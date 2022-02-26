ed658415b5466052b1829737e76371c9e43819db
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-02-26 00:49:18 -0500

    no covid diagnosis in negative group

--- preprocess/helper_covid4manu.sh
+++ preprocess/helper_covid4manu.sh
@@ -1,5 +1,5 @@
-python pre_cohort_manuscript.py --dataset COL 2>&1 | tee  log/pre_cohort_manuscript_COL.txt
-python pre_cohort_manuscript.py --dataset WCM 2>&1 | tee  log/pre_cohort_manuscript_WCM.txt
-python pre_cohort_manuscript.py --dataset NYU 2>&1 | tee  log/pre_cohort_manuscript_NYU.txt
-python pre_cohort_manuscript.py --dataset MONTE 2>&1 | tee  log/pre_cohort_manuscript_MONTE.txt
-python pre_cohort_manuscript.py --dataset MSHS 2>&1 | tee  log/pre_cohort_manuscript_MSHS.txt
+python pre_cohort_manuscript.py --dataset COL 2>&1 | tee  log/pre_cohort_manuscript_NegNoCovid_COL.txt
+python pre_cohort_manuscript.py --dataset WCM 2>&1 | tee  log/pre_cohort_manuscript_NegNoCovid_WCM.txt
+python pre_cohort_manuscript.py --dataset NYU 2>&1 | tee  log/pre_cohort_manuscript_NegNoCovid_NYU.txt
+python pre_cohort_manuscript.py --dataset MONTE 2>&1 | tee  log/pre_cohort_manuscript_NegNoCovid_MONTE.txt
+python pre_cohort_manuscript.py --dataset MSHS 2>&1 | tee  log/pre_cohort_manuscript_NegNoCovid_MSHS.txt
