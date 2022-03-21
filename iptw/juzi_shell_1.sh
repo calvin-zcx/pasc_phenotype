f41327054de081d65fbf38d6cb205f718ad79112
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-03-21 14:58:51 -0400

    updated shells

--- iptw/juzi_shell_1.sh
+++ iptw/juzi_shell_1.sh
@@ -1,9 +1,8 @@
 mkdir log
-taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity less65 2>&1 | tee  log/screen_dx_insight_ALL_less65.txt
-taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity male 2>&1 | tee  log/screen_dx_insight_ALL_male.txt
-taskset --cpu-list 0-3 python screen_dx_v2.py --dataset oneflorida --site all --severity 65to75 2>&1 | tee  log/screen_dx_oneflorida_all_65to75.txt
-taskset --cpu-list 0-3 python screen_dx_v2.py --dataset oneflorida --site all --severity white 2>&1 | tee  log/screen_dx_oneflorida_all_white.txt
-
+taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity all 2>&1 | tee  log/screen_dx_insight_ALL_all.txt
+taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity icu 2>&1 | tee  log/screen_dx_insight_ALL_icu.txt
+taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity inpatient 2>&1 | tee  log/screen_dx_insight_ALL_inpatient.txt
+taskset --cpu-list 0-3 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity outpatient 2>&1 | tee  log/screen_dx_insight_ALL_outpatient.txt
 
 
 
--- iptw/juzi_shell_3.sh
+++ iptw/juzi_shell_3.sh
@@ -1,4 +1,5 @@
 mkdir log
-taskset --cpu-list 8-11 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity 75above 2>&1 | tee  log/screen_dx_insight_ALL_75above.txt
-taskset --cpu-list 8-11 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity black 2>&1 | tee  log/screen_dx_insight_ALL_black.txt
-taskset --cpu-list 8-11 python screen_dx_v2.py --dataset oneflorida --site all --severity female 2>&1 | tee  log/screen_dx_oneflorida_all_female.txt
+taskset --cpu-list 8-11 python screen_dx_v2.py --dataset oneflorida --site all --severity all 2>&1 | tee  log/screen_dx_oneflorida_ALL_all.txt
+taskset --cpu-list 8-11 python screen_dx_v2.py --dataset oneflorida --site all --severity icu 2>&1 | tee  log/screen_dx_oneflorida_ALL_icu.txt
+taskset --cpu-list 8-11 python screen_dx_v2.py --dataset oneflorida --site all --severity inpatient 2>&1 | tee  log/screen_dx_oneflorida_ALL_inpatient.txt
+taskset --cpu-list 8-11 python screen_dx_v2.py --dataset oneflorida --site all --severity outpatient 2>&1 | tee  log/screen_dx_oneflorida_ALL_outpatient.txt
