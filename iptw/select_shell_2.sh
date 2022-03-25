c0d05c98c92ad18843c22a70c5c4176fd3416656
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-03-25 12:24:25 -0400

    cif_1_w_CILower = ajf1w_CI[ajf1w_CI.columns[0]].asof(point_in_time).to_numpy()

--- iptw/select_shell_2.sh
+++ iptw/select_shell_2.sh
@@ -9,10 +9,10 @@ taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL -
 taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity T2D-Obesity --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_T2D-Obesity-selectpasc.txt
 taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Mental-substance --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Mental-substance-selectpasc.txt
 taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Anemia --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Anemia-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Arrythmia --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Arrythmia-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CKD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CKD-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CPD-COPD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CPD-COPD-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CAD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CAD-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Hypertension --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Hypertension-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Corticosteroids --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Corticosteroids-selectpasc.txt
-taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity healthy --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_healthy-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Arrythmia --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Arrythmia-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CKD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CKD-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CPD-COPD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CPD-COPD-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity CAD --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_CAD-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Hypertension --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Hypertension-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity Corticosteroids --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_Corticosteroids-selectpasc.txt
+#taskset --cpu-list 4-7 python screen_dx_v2.py --dataset V15_COVID19 --site ALL --severity healthy --selectpasc 2>&1 | tee  log/screen_dx_insight_ALL_healthy-selectpasc.txt
