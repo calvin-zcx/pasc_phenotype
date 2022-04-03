56a3d526a5795bda46dd6df268ba86ab5212d41d
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-04-03 10:48:23 -0400

    -4negctrl dx med infor

--- iptw/select_shell_1.sh
+++ iptw/select_shell_1.sh
@@ -1,13 +1,13 @@
 mkdir log
 # taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity all 2>&1 | tee  log/screen_dx_pooled_all-competingRiskCumInc.txt
-taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity icu 2>&1 | tee  log/screen_dx_pooled_icu-competingRiskCumInc.txt
-taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity inpatient 2>&1 | tee  log/screen_dx_pooled_inpatient-competingRiskCumInc.txt
-taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity outpatient 2>&1 | tee  log/screen_dx_pooled_outpatient-competingRiskCumInc.txt
+#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity icu 2>&1 | tee  log/screen_dx_pooled_icu-competingRiskCumInc.txt
+#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity inpatient 2>&1 | tee  log/screen_dx_pooled_inpatient-competingRiskCumInc.txt
+#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity outpatient 2>&1 | tee  log/screen_dx_pooled_outpatient-competingRiskCumInc.txt
 taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity less65 2>&1 | tee  log/screen_dx_pooled_less65-competingRiskCumInc.txt
 taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity above65 2>&1 | tee  log/screen_dx_pooled_above65-competingRiskCumInc.txt
-#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 20to40 2>&1 | tee  log/screen_dx_pooled_20to40-competingRiskCumInc.txt
-#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 40to55 2>&1 | tee  log/screen_dx_pooled_40to55-competingRiskCumInc.txt
-#taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 55to65 2>&1 | tee  log/screen_dx_pooled_55to65-competingRiskCumInc.txt
+taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 20to40 2>&1 | tee  log/screen_dx_pooled_20to40-competingRiskCumInc.txt
+taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 40to55 2>&1 | tee  log/screen_dx_pooled_40to55-competingRiskCumInc.txt
+taskset --cpu-list 0-3 python screen_dx_pooled.py --dataset pooled --severity 55to65 2>&1 | tee  log/screen_dx_pooled_55to65-competingRiskCumInc.txt
 
 
 
