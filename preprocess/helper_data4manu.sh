1c388e601b4c33df840ebc9e05d9891664f4e6ac
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-04-08 16:44:49 -0400

    pre matrix into more dim

--- preprocess/helper_data4manu.sh
+++ preprocess/helper_data4manu.sh
@@ -1,3 +1,4 @@
 python pre_data_manuscript.py --dataset ALL --cohorts covid_4manuscript 2>&1 | tee  log/pre_data_manuscript.txt
 python pre_data_manuscript.py --dataset ALL --cohorts covid_4manuNegNoCovid 2>&1 | tee  log/pre_data_manuscript_NegNoCovid.txt
 python pre_data_manuscript_negctrl.py --dataset ALL --cohorts covid_4manuNegNoCovid 2>&1 | tee  log/pre_data_manuscript_negctrl_NegNoCovid.txt
+python pre_data_manuscript.py --dataset ALL --cohorts covid_4manuNegNoCovidV2 2>&1 | tee  log/pre_data_manuscript_NegNoCovidV2.txt
