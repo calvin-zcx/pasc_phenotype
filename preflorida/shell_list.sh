f03def3df95afd124fba3c4a5658594689009077
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-04-10 21:31:08 -0400

    vital for florida, must dump chunk 12

--- preflorida/shell_list.sh
+++ preflorida/shell_list.sh
@@ -4,9 +4,10 @@ mkdir log
 #python pre_demo.py --dataset all 2>&1 | tee  log/pre_demo_allcombined.txt
 #python pre_covid_lab.py --dataset all 2>&1 | tee  log/pre_covid_lab_allcombined.txt
 #python pre_diagnosis.py --dataset all 2>&1 | tee  log/pre_diagnosis_all.txt
-python pre_medication.py --dataset all 2>&1 | tee  log/pre_medication_all.txt
-python pre_encounter.py --dataset all 2>&1 | tee  log/pre_encounter_all.txt
-python pre_procedure.py --dataset all 2>&1 | tee  log/pre_procedure_all.txt
-python pre_immun.py --dataset all 2>&1 | tee  log/pre_immun_all.txt
-python pre_death.py --dataset all 2>&1 | tee  log/pre_death_all.txt
-python pre_cohort_manuscript.py --dataset all 2>&1 | tee  log/pre_cohort_manuscript_all_oneflorida.txt
+#python pre_medication.py --dataset all 2>&1 | tee  log/pre_medication_all.txt
+#python pre_encounter.py --dataset all 2>&1 | tee  log/pre_encounter_all.txt
+#python pre_procedure.py --dataset all 2>&1 | tee  log/pre_procedure_all.txt
+#python pre_immun.py --dataset all 2>&1 | tee  log/pre_immun_all.txt
+#python pre_death.py --dataset all 2>&1 | tee  log/pre_death_all.txt
+python pre_vital.py --dataset all 2>&1 | tee  log/pre_vital_all.txt
+python pre_cohort_manuscript.py --dataset all 2>&1 | tee  log/pre_cohort_manuscriptV2_all_oneflorida.txt
