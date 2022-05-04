28354d46f91a2ea6475569831bedafdc0b0b600d
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-05-04 12:40:13 -0400

    inpatient and outpatient

--- prediction/help_cmd_1.sh
+++ prediction/help_cmd_1.sh
@@ -2,6 +2,10 @@
 #python screen_risk_factors.py --dataset INSIGHT --goal allpasc 2>&1 | tee  log/screen_risk_factors-insight-elix-allpasc.txt
 #python screen_risk_factors.py --dataset INSIGHT --goal anyorgan 2>&1 | tee  log/screen_risk_factors-insight-elix-anyorgan.txt
 #python screen_risk_factors.py --dataset INSIGHT --goal allorgan 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan.txt
+python screen_risk_factors.py --dataset INSIGHT --goal anypasc --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-anypasc-positive.txt
+python screen_risk_factors.py --dataset INSIGHT --goal allpasc --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-allpasc-positive.txt
+python screen_risk_factors.py --dataset INSIGHT --goal anyorgan --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-anyorgan-positive.txt
+python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-positive.txt
 python screen_risk_factors.py --dataset INSIGHT --goal anypasc --population negative 2>&1 | tee  log/screen_risk_factors-insight-elix-anypasc-negative.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allpasc --population negative 2>&1 | tee  log/screen_risk_factors-insight-elix-allpasc-negative.txt
 python screen_risk_factors.py --dataset INSIGHT --goal anyorgan --population negative 2>&1 | tee  log/screen_risk_factors-insight-elix-anyorgan-negative.txt
@@ -10,7 +14,3 @@ python screen_risk_factors.py --dataset INSIGHT --goal anypasc --population all 
 python screen_risk_factors.py --dataset INSIGHT --goal allpasc --population all 2>&1 | tee  log/screen_risk_factors-insight-elix-allpasc-all.txt
 python screen_risk_factors.py --dataset INSIGHT --goal anyorgan --population all 2>&1 | tee  log/screen_risk_factors-insight-elix-anyorgan-all.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all.txt
-python screen_risk_factors.py --dataset INSIGHT --goal anypasc --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-anypasc-positive.txt
-python screen_risk_factors.py --dataset INSIGHT --goal allpasc --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-allpasc-positive.txt
-python screen_risk_factors.py --dataset INSIGHT --goal anyorgan --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-anyorgan-positive.txt
-python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-positive.txt
