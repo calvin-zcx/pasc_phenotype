77b2507c10a6356fdeb22c3b342bc72ddc259c29
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-05-05 14:38:11 -0400

    cox_data = cov_df[['T', 'E', index] + [x for x in adjusted_col if ((x != index) and (x in cov_df.columns))]]

--- prediction/help_cmd_4.sh
+++ prediction/help_cmd_4.sh
@@ -1,10 +1,10 @@
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population positive 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-positive.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population negative 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-negative.txt
-python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all.txt
+#python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population positive --severity inpatienticu 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-positive-inpatienticu.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population negative --severity inpatienticu 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-negative-inpatienticu.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all --severity inpatienticu 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all-inpatienticu.txt
 python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population positive --severity outpatient 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-positive-outpatient.txt
-python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population negative --severity outpatient 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-negative-outpatient.txt
-python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all --severity outpatient 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all-outpatient.txt
+#python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population negative --severity outpatient 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-negative-outpatient.txt
+#python screen_risk_factors.py --dataset INSIGHT --goal allorgan --population all --severity outpatient 2>&1 | tee  log/screen_risk_factors-insight-elix-allorgan-all-outpatient.txt
 
