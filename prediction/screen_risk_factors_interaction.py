60fff7c184ddef77d6ad7f83866231954ae4b350
Calvin Zang <calvin-zcx@users.noreply.github.com>
2022-05-28 01:05:21 -0400

    check interaction term

--- prediction/screen_risk_factors_interaction.py
+++ prediction/screen_risk_factors_interaction.py
@@ -48,7 +48,7 @@ def parse_args():
                                            'anypascsevere', 'anypascmoderate'],
                         default='anypasc')
     parser.add_argument("--random_seed", type=int, default=0)
-    parser.add_argument('--negative_ratio', type=int, default=4)
+    parser.add_argument('--negative_ratio', type=int, default=2)
 
     args = parser.parse_args()
 
