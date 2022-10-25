db7a7e6b7bfc42e1204e1f696e8b85d591c57ff0
chz4001 <chz4001@med.cornell.edu>
2022-10-25 00:58:06 +0000

    ec pd to datetime; pre cohort

--- prerecover/shell_list_recover.ps1
+++ prerecover/shell_list_recover.ps1
@@ -1,11 +1,22 @@
-#python pre_lab.py --dataset wcm 2>&1 | tee  log\pre_lab_wcm.txt
-#python pre_demo.py --dataset wcm 2>&1 | tee  log\pre_demo_wcm.txt
-#python pre_covid_lab.py --dataset wcm 2>&1 | tee  log\pre_covid_lab_wcm.txt
-#python pre_diagnosis.py --dataset wcm 2>&1 | tee  log/pre_diagnosis_wcm.txt
-#python pre_medication.py --dataset wcm 2>&1 | tee  log/pre_medication_wcm.txt
-#python pre_encounter.py --dataset wcm 2>&1 | tee  log/pre_encounter_wcm.txt
-#python pre_procedure.py --dataset wcm 2>&1 | tee  log/pre_procedure_wcm.txt
+python pre_lab.py --dataset wcm 2>&1 | tee  log\pre_lab_wcm.txt
+python pre_demo.py --dataset wcm 2>&1 | tee  log\pre_demo_wcm.txt
+python pre_covid_lab.py --dataset wcm 2>&1 | tee  log\pre_covid_lab_wcm.txt
+python pre_diagnosis.py --dataset wcm 2>&1 | tee  log/pre_diagnosis_wcm.txt
+python pre_medication.py --dataset wcm 2>&1 | tee  log/pre_medication_wcm.txt
+python pre_encounter.py --dataset wcm 2>&1 | tee  log/pre_encounter_wcm.txt
+python pre_procedure.py --dataset wcm 2>&1 | tee  log/pre_procedure_wcm.txt
 python pre_immun.py --dataset wcm 2>&1 | tee  log/pre_immun_wcm.txt
 python pre_death.py --dataset wcm 2>&1 | tee  log/pre_death_wcm.txt
- python pre_vital.py --dataset wcm 2>&1 | tee  log/pre_vital_wcm.txt
-# python pre_cohort_manuscript.py --dataset all 2>&1 | tee  log/pre_cohort_manuscriptV2_all_oneflorida.txt
+python pre_vital.py --dataset wcm 2>&1 | tee  log/pre_vital_wcm.txt
+python pre_cohort_manuscript.py --dataset wcm 2>&1 | tee  log/pre_cohort_manuscript_wcm.txt
+python pre_lab.py --dataset columbia 2>&1 | tee  log\pre_lab_columbia.txt
+python pre_demo.py --dataset columbia 2>&1 | tee  log\pre_demo_columbia.txt
+python pre_covid_lab.py --dataset columbia 2>&1 | tee  log\pre_covid_lab_columbia.txt
+python pre_diagnosis.py --dataset columbia 2>&1 | tee  log/pre_diagnosis_columbia.txt
+python pre_medication.py --dataset columbia 2>&1 | tee  log/pre_medication_columbia.txt
+python pre_encounter.py --dataset columbia 2>&1 | tee  log/pre_encounter_columbia.txt
+python pre_procedure.py --dataset columbia 2>&1 | tee  log/pre_procedure_columbia.txt
+python pre_immun.py --dataset columbia 2>&1 | tee  log/pre_immun_columbia.txt
+python pre_death.py --dataset columbia 2>&1 | tee  log/pre_death_columbia.txt
+python pre_vital.py --dataset columbia 2>&1 | tee  log/pre_vital_columbia.txt
+python pre_cohort_manuscript.py --dataset columbia 2>&1 | tee  log/pre_cohort_manuscript_columbia.txt
