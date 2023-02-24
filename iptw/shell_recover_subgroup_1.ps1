554810f31aff4129d5d7c461bbfd80e1f9861709
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2023-02-24 13:06:15 -0500

    subgroup runing

--- iptw/shell_recover_subgroup_1.ps1
+++ iptw/shell_recover_subgroup_1.ps1
@@ -1,4 +1,5 @@
 mkdir log_recover
+python screen_dx_recover.py --site all --severity inpatienticu --negative_ratio 1 --downsample_ratio 1 2>&1 | tee  log_recover/screen_dx_recover_all_inpatienticu_neg1_downsample1.txt
 python screen_dx_recover.py --site all --severity inpatient --negative_ratio 1 --downsample_ratio 1 2>&1 | tee  log_recover/screen_dx_recover_all_inpatient_neg1_downsample1.txt
 python screen_dx_recover.py --site all --severity icu --negative_ratio 1 --downsample_ratio 1 2>&1 | tee  log_recover/screen_dx_recover_all_icu_neg1_downsample1.txt
 python screen_dx_recover.py --site all --severity deltaAndBefore --negative_ratio 1 --downsample_ratio 1 2>&1 | tee  log_recover/screen_dx_recover_all_deltaAndBefore_neg1_downsample1.txt
