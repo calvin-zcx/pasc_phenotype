c6be3b3a32b6371118c165873306a46ba107f054
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2023-03-29 10:24:19 -0400

    pregnancy control cohort type2, deleted unbalanced/not reseaonable features, 1:10, spline n knots 5, not using obc comorbiditis, age stratified sampling, also stratified by index date stractum

--- specific/any_pasc_summary.py
+++ specific/any_pasc_summary.py
@@ -18,7 +18,9 @@ import multipy.fdr as fdr
 print = functools.partial(print, flush=True)
 import time
 
-if __name__ == '__main__':
+
+def old_version():
+    # 2023-3-23
     infile = r'../data/recover/output/results/DX-all-neg1.0/causal_effects_specific-all-neg1_compare_with_others.xlsx'
     outfile = infile.replace('.xlsx', '_aux_sum_selection.xlsx')
     df = pd.read_excel(infile, sheet_name='dx')
@@ -26,16 +28,16 @@ if __name__ == '__main__':
     apx_vec = ['', ' deltaAndBefore', ' omicron', ' inpatienticu']
 
     for apx in apx_vec:
-        hr = df['hr-w'+apx]
-        bf = df['bool_bonf'+apx]
-        by = df['bool_by'+apx]
-        top_bf = (((hr)>1) & (bf==1)).astype('int')
-        top_by = (((hr)>1) & (by==1)).astype('int')
-        df['risk_bf'+apx] = top_bf
+        hr = df['hr-w' + apx]
+        bf = df['bool_bonf' + apx]
+        by = df['bool_by' + apx]
+        top_bf = (((hr) > 1) & (bf == 1)).astype('int')
+        top_by = (((hr) > 1) & (by == 1)).astype('int')
+        df['risk_bf' + apx] = top_bf
         df['risk_by' + apx] = top_by
 
-    df['risk_bf_sum'] = df[['risk_bf'+apx for apx in apx_vec]].sum(axis = 1)
-    df['risk_by_sum'] = df[['risk_by'+apx for apx in apx_vec]].sum(axis = 1)
+    df['risk_bf_sum'] = df[['risk_bf' + apx for apx in apx_vec]].sum(axis=1)
+    df['risk_by_sum'] = df[['risk_by' + apx for apx in apx_vec]].sum(axis=1)
 
     df['risk_bf_sum+narrow'] = df['risk_bf_sum'] + df['selected_narrow_25'].fillna(0)
     df['risk_bf_sum+broad'] = df['risk_bf_sum'] + df['selected_broad44'].fillna(0)
@@ -43,5 +45,27 @@ if __name__ == '__main__':
     df['risk_by_sum+broad'] = df['risk_by_sum'] + df['selected_broad44'].fillna(0)
 
     df.to_excel(outfile, sheet_name='dx')
+
+    print('Done')
+
+
+if __name__ == '__main__':
+
+    apx_vec = ['deltaAndBeforeoutpatient', 'deltaAndBeforeinpatienticu', 'omicronoutpatient', 'omicroninpatienticu', ]
+
+    for apx in apx_vec:
+        infile = r'../data/recover/output/results/DX-{}-neg1.0/causal_effects_specific-{}.xlsx'.format(apx, apx)
+        outfile = infile.replace('.xlsx', '_add_selection.xlsx')
+        df = pd.read_excel(infile, sheet_name='dx')
+
+        hr = df['hr-w']
+        bf = df['bool_bonf']
+        by = df['bool_by' ]
+        top_bf = (((hr) > 1) & (bf == 1)).astype('int')
+        top_by = (((hr) > 1) & (by == 1)).astype('int')
+        df['risk_bf' + apx] = top_bf
+        df['risk_by' + apx] = top_by
+        df.to_excel(outfile, sheet_name='dx')
+        print('Done')
 
     print('Done')
