6cae14998c4fc7d6f9bd01947731a43fef724e21
chz4001 <chz4001@med.cornell.edu>
2023-06-02 19:18:33 +0000

    select covid positive

--- prerecover/shell_generate.py
+++ prerecover/shell_generate.py
@@ -110,8 +110,7 @@ def shell_2023_4_6():
     utils.split_shell_file(r"output\shells\shell_all_rerun.ps1", divide=5, skip_first=0)
     print('Done! Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
 
-
-if __name__ == '__main__':
+def shell_2023_6_2():
     # python pre_codemapping.py 2>&1 | tee  log/pre_codemapping_zip_adi.txt
     start_time = time.time()
 
@@ -125,10 +124,45 @@ if __name__ == '__main__':
         for i, site in enumerate(site_list):
             site = site.strip()
             cmdstr = """python pre_data_manuscript_withAllDays_preg_addCoexist.py --dataset nyu --cohorts covid_4manuNegNoCovidV2age18 2>&1 | tee  log/pre_data_manuscript_withAllDays_preg_addCoexist_nyu.txt
-""".replace('nyu', site)
+    """.replace('nyu', site)
             f.write(cmdstr)
 
             print(i, site, 'done')
 
     utils.split_shell_file(r"output\shells\shell_all_pregcoexist.ps1", divide=6, skip_first=0)
+    print('Done! Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+
+
+if __name__ == '__main__':
+    # python pre_codemapping.py 2>&1 | tee  log/pre_codemapping_zip_adi.txt
+    start_time = time.time()
+
+    df_site = pd.read_excel('RECOVER Adult Site schemas_edit.xlsx')
+
+    site_list = df_site.loc[df_site['selected'] == 1, 'Schema name']
+
+    print('site_list:', len(site_list), site_list)
+
+    with open(r'output\shells\shell_all_2023-6.ps1', 'wt') as f:
+        for i, site in enumerate(site_list):
+            site = site.strip()
+            cmdstr = """python pre_lab.py --dataset emory 2>&1 | tee  log\pre_lab_emory_2023_6.txt
+python pre_demo.py --dataset emory 2>&1 | tee  log\pre_demo_emory_2023_6.txt
+python pre_covid_lab.py --dataset emory 2>&1 | tee  log\pre_covid_lab_emory_2023_6.txt
+python pre_diagnosis.py --dataset emory 2>&1 | tee  log/pre_diagnosis_emory_2023_6.txt
+python pre_medication.py --dataset emory 2>&1 | tee  log/pre_medication_emory_2023_6.txt
+python pre_encounter.py --dataset emory 2>&1 | tee  log/pre_encounter_emory_2023_6.txt
+python pre_procedure.py --dataset emory 2>&1 | tee  log/pre_procedure_emory_2023_6.txt
+python pre_immun.py --dataset emory 2>&1 | tee  log/pre_immun_emory_2023_6.txt
+python pre_death.py --dataset emory 2>&1 | tee  log/pre_death_emory_2023_6.txt
+python pre_vital.py --dataset emory 2>&1 | tee  log/pre_vital_emory_2023_6.txt
+python pre_cohort_manuscript_age18.py --dataset emory 2>&1 | tee  log/pre_cohort_manuscript_age18_emory_2023_6.txt
+python pre_data_manuscript_withAllDays.py --cohorts covid_4manuNegNoCovidV2age18 --dataset emory 2>&1 | tee  log\pre_data_manuscript_withAllDays_emory_2023_6.txt
+python pre_data_manuscript_withAllDays_preg_addCoexist.py --dataset emory --cohorts covid_4manuNegNoCovidV2age18 2>&1 | tee  log/pre_data_manuscript_withAllDays_preg_addCoexist_emory_2023_6.txt
+""".replace('emory', site)
+            f.write(cmdstr)
+
+            print(i, site, 'done')
+
+    # utils.split_shell_file(r"output\shells\shell_all_2023-6.ps1", divide=4, skip_first=0)
     print('Done! Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
