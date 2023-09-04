917b91558ff323c29d401fe0c4914a98359e97e3
chz4001 <chz4001@med.cornell.edu>
2023-09-04 21:06:33 +0000

    shell generate

--- prerecover/shell_generate.py
+++ prerecover/shell_generate.py
@@ -183,15 +183,17 @@ def shell_lab_dx_med_4covid():
 #python pre_dx_4covid.py --dataset nyu 2>&1 | tee  log\pre_dx_4covid_nyu.txt
 python pre_med_4covid.py --dataset nyu 2>&1 | tee  log\pre_med_4covid_nyu.txt
 #python pre_demo.py --dataset nyu 2>&1 | tee  log\pre_demo_nyu.txt
+python pre_covid_records.py --dataset nyu 2>&1 | tee  log\pre_covid_records_nyu.txt
 """.replace('nyu', site)
             f.write(cmdstr)
             print(i, site, 'done')
 
     utils.split_shell_file(r"shell_all_202309.ps1", divide=5, skip_first=0)
     print('Done! Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+    # python pre_covid_lab.py --dataset nyu 2>&1 | tee  log\pre_covid_lab_nyu.txt
+    # not using this, change to pre_covid_records.py
 
     """
-    python pre_covid_lab.py --dataset nyu 2>&1 | tee  log\pre_covid_lab_nyu.txt
     python pre_diagnosis.py --dataset nyu 2>&1 | tee  log/pre_diagnosis_nyu.txt
     python pre_medication.py --dataset nyu 2>&1 | tee  log/pre_medication_nyu.txt
     python pre_encounter.py --dataset nyu 2>&1 | tee  log/pre_encounter_nyu.txt
