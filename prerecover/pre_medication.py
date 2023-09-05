802c0948c6319091f64374de4929b322f2b24009
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2023-09-05 15:06:18 -0400

    pre_medication add dispensing table # Cautions: need to add error handle inside. If med_admi is empty, but dispensing is not, error break will ignore     # dispense data

--- prerecover/pre_medication.py
+++ prerecover/pre_medication.py
@@ -16,6 +16,7 @@ print = functools.partial(print, flush=True)
 from collections import defaultdict
 from datetime import datetime, date
 
+
 # 1. preprocess prescribing file, support supply day calculation and imputation
 # 2. preprocess med_admin file if necessary, support supply day calculation and imputation
 # 3. combine results from both files if necessary (for COL case)
@@ -23,20 +24,26 @@ from datetime import datetime, date
 
 def parse_args():
     parser = argparse.ArgumentParser(description='preprocess medication table')
-    parser.add_argument('--dataset', default='ochsner', help='site dataset')
+    parser.add_argument('--dataset', default='nebraska', help='site dataset')
     args = parser.parse_args()
 
-    args.patient_list_file = r'../data/recover/output/{}/patient_covid_lab_{}.pkl'.format(args.dataset, args.dataset)
+    # use all selected patients by lab-dx-med, selected covid-CP specific cohorts later in pre_cohort_**.py
+    args.patient_list_file = r'../data/recover/output/{}/patient_covid_lab-dx-med_{}.pkl'.format(args.dataset, args.dataset)
+    # args.patient_list_file = r'../data/recover/output/{}/patient_covid_lab_{}.pkl'.format(args.dataset, args.dataset)
+    # patient_covid_lab-dx-med_mcw
+
     args.med_admin_file = r'{}.med_admin'.format(args.dataset)
     args.prescribe_file = r'{}.prescribing'.format(args.dataset)
+    args.dispensing_file = r'{}.dispensing'.format(args.dataset)
 
     args.med_admin_output = r'../data/recover/output/{}/_med_admin_{}.pkl'.format(args.dataset, args.dataset)
     args.prescribe_output = r'../data/recover/output/{}/_prescribing_{}.pkl'.format(args.dataset, args.dataset)
+    args.dispensing_output = r'../data/recover/output/{}/_dispensing_{}.pkl'.format(args.dataset, args.dataset)
+
     args.output_file = r'../data/recover/output/{}/medication_{}.pkl'.format(args.dataset, args.dataset)
 
     print('args:', args)
     return args
-
 
 
 def read_prescribing(input_file, output_file='', selected_patients={}):
@@ -80,7 +87,7 @@ def read_prescribing(input_file, output_file='', selected_patients={}):
         stream_results=True, max_row_buffer=chunksize
     )
 
-    id_med = defaultdict(set)
+    id_med = defaultdict(list)
     i = 0
     n_rows = 0
     dfs = []
@@ -148,13 +155,15 @@ def read_prescribing(input_file, output_file='', selected_patients={}):
                 n_discard_row += 1
             else:
                 if not selected_patients:
-                    id_med[patid].add(
-                        (start_date, rxnorm, days, encid, 'pr'))  # 2022-10-24 add encounter id and prescribeing table id
+                    id_med[patid].append(
+                        (
+                        start_date, rxnorm, days, encid, 'pr'))  # 2022-10-24 add encounter id and prescribeing table id
                     n_recorded_row += 1
                 else:
                     if patid in selected_patients:
-                        id_med[patid].add(
-                            (start_date, rxnorm, days, encid, 'pr'))  # 2022-10-24 add encounter id and prescribeing table id
+                        id_med[patid].append(
+                            (start_date, rxnorm, days, encid,
+                             'pr'))  # 2022-10-24 add encounter id and prescribeing table id
                         n_recorded_row += 1
                     else:
                         n_not_in_list_row += 1
@@ -181,7 +190,7 @@ def read_prescribing(input_file, output_file='', selected_patients={}):
     # sort
     print('Sort med_list in id_med by time')
     for patid, med_list in id_med.items():
-        med_list_sorted = sorted(med_list, key=lambda x: x[0])
+        med_list_sorted = sorted(set(med_list), key=lambda x: x[0])
         id_med[patid] = med_list_sorted
 
     print('Total Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
@@ -237,7 +246,7 @@ def read_med_admin(input_file, output_file='', selected_patients={}):
         stream_results=True, max_row_buffer=chunksize
     )
 
-    id_med = defaultdict(set)
+    id_med = defaultdict(list)
     i = 0
     n_rows = 0
     dfs = []
@@ -308,12 +317,12 @@ def read_med_admin(input_file, output_file='', selected_patients={}):
                 n_discard_row += 1
             else:
                 if not selected_patients:
-                    id_med[patid].add(
+                    id_med[patid].append(
                         (start_date, rxnorm, days, encid, 'ma'))  # 2022-10-24 add encounter id and med_admi table id
                     n_recorded_row += 1
                 else:
                     if patid in selected_patients:
-                        id_med[patid].add((start_date, rxnorm, days, encid, 'ma'))
+                        id_med[patid].append((start_date, rxnorm, days, encid, 'ma'))
                         n_recorded_row += 1
                     else:
                         n_not_in_list_row += 1
@@ -346,7 +355,168 @@ def read_med_admin(input_file, output_file='', selected_patients={}):
     # sort
     print('sort med_list in id_med by time')
     for patid, med_list in id_med.items():
-        med_list_sorted = sorted(med_list, key=lambda x: x[0])
+        med_list_sorted = sorted(set(med_list), key=lambda x: x[0])
+        id_med[patid] = med_list_sorted
+
+    print('Total Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+
+    if output_file:
+        utils.check_and_mkdir(output_file)
+        pickle.dump(id_med, open(output_file, 'wb'))
+        print('dump done to {}'.format(output_file))
+
+    print('Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+    return id_med
+
+
+def read_dispensing(input_file, output_file='', selected_patients={}):
+    """
+    :param selected_patients:
+    :param output_file:
+    :param input_file: input dispensing file with std format
+    :param out_file: output id_code-list[patid] = [(start_time, rxnorm, days), ...]  sorted by start_time pickle
+    :return: id_code-list[patid] = [(start_time, rxnorm, days), ...]  sorted by start_time pickle
+    :Notice:
+        discard rows with NULL start_date or rxnorm
+        Only applied to COL data, no WCM
+        1.COL data: e.g:
+        df.shape: (6890724, 32)
+
+        2. WCM data: e.g.
+        df.shape: (0, 21)
+    """
+    start_time = time.time()
+    print('In read_dispensing, input_file:', input_file, 'output_file:', output_file)
+    if selected_patients:
+        print('using selected_patients, len(selected_patients):', len(selected_patients))
+
+    chunksize = 100000
+    # sasds = pd.read_sas(input_file,
+    #                     encoding='WINDOWS-1252',
+    #                     chunksize=chunksize,
+    #                     iterator=True)
+
+    connect_string, cred_dict = load_sql_credential()
+    table_name = input_file
+    table_size = get_table_size(connect_string, table_name)
+    table_rows = get_table_rows(connect_string, table_name)
+    print('Read sql table:', table_name, '| Table size:', table_size, '| No. of rows:', table_rows)
+    n_chunk = int(np.ceil(table_rows / chunksize))
+    sql_query = """select * from {};
+                       """.format(table_name)
+
+    print('read:', sql_query)
+    engine = create_engine(connect_string)
+    connection = engine.connect().execution_options(
+        stream_results=True, max_row_buffer=chunksize
+    )
+
+    id_med = defaultdict(list)
+    i = 0
+    n_rows = 0
+    dfs = []
+
+    n_no_rxnorm = 0
+    n_no_date = 0
+    n_no_days_supply = 0
+
+    n_discard_row = 0
+    n_recorded_row = 0
+    n_not_in_list_row = 0
+
+    for chunk in tqdm(pd.read_sql(sql_query, connection, chunksize=chunksize), total=n_chunk):
+        i += 1
+        if chunk.empty:
+            print("ERROR: Empty chunk! break!")
+            break
+        n_rows += len(chunk)
+        chunk.rename(columns=lambda x: x.upper(), inplace=True)
+        if i == 1:
+            print('chunk.shape', chunk.shape)
+            print('chunk.columns', chunk.columns)
+
+        for index, row in chunk.iterrows():
+            patid = row['PATID']
+            rx_start_date = clean_date_str(row['DISPENSE_DATE'])
+            rxnorm = row['NDC']
+            rx_days = row['DISPENSE_SUP']
+
+            if 'RAW_NDC' in row.index:
+                raw_rxnorm = row['RAW_NDC']
+                if isinstance(rxnorm, str):
+                    if raw_rxnorm.upper().startswith('NDC:'):
+                        raw_rxnorm = raw_rxnorm[4:]
+            else:
+                raw_rxnorm = np.nan
+
+            encid = ''  # row['ENCOUNTERID']  # no enc id in dispense table
+            dispense_source = row['DISPENSE_SOURCE']
+
+            # start_date
+            if pd.notna(rx_start_date):
+                start_date = rx_start_date
+            else:
+                start_date = np.nan
+                n_no_date += 1
+
+            if pd.isna(rxnorm):
+                if pd.notna(raw_rxnorm):
+                    rxnorm = raw_rxnorm
+                else:
+                    n_no_rxnorm += 1
+                    rxnorm = np.nan
+
+            # days supply
+            if pd.notna(rx_days) and rx_days:
+                days = int(float(rx_days))
+            else:
+                days = -1
+                n_no_days_supply += 1
+
+            if pd.isna(rxnorm) or pd.isna(start_date):
+                n_discard_row += 1
+            else:
+                if not selected_patients:
+                    # replace enc_id to dispense_source, no enc_id in dispense table
+                    id_med[patid].append(
+                        (start_date, rxnorm, days, dispense_source, 'ds'))
+                    n_recorded_row += 1
+                else:
+                    if patid in selected_patients:
+                        id_med[patid].append((start_date, rxnorm, days, dispense_source, 'ds'))
+                        n_recorded_row += 1
+                    else:
+                        n_not_in_list_row += 1
+
+        dfs.append(chunk[['DISPENSE_DATE']])
+        if i % 25 == 0:
+            print('chunk:', i, 'time:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
+            print('n_rows:', n_rows,
+                  'n_no_rxnorm:', n_no_rxnorm, 'n_no_date:', n_no_date, 'n_no_days_supply:', n_no_days_supply,
+                  'n_discard_row:', n_discard_row, 'n_recorded_row:', n_recorded_row, 'n_not_in_list_row:',
+                  n_not_in_list_row)
+
+    print('n_rows:', n_rows, '#chunk: ', i, 'chunk size:', chunksize)
+    print('n_rows:', n_rows,
+          'n_no_rxnorm:', n_no_rxnorm, 'n_no_date:', n_no_date, 'n_no_days_supply:', n_no_days_supply,
+          'n_discard_row:', n_discard_row, 'n_recorded_row:', n_recorded_row, 'n_not_in_list_row:', n_not_in_list_row)
+
+    print('len(id_med):', len(id_med))
+    try:
+        dfs = pd.concat(dfs)
+        print('dfs.shape', dfs.shape)
+        print('Time range of dispensing table  of selected patients [DISPENSE_DATE]:',
+              pd.to_datetime(dfs["DISPENSE_DATE"]).describe(datetime_is_numeric=True))
+    except Exception as e:
+        # empty file, empty list, nothing to concatenate
+        #  raise ValueError("No objects to concatenate")
+        # ValueError: No objects to concatenate
+        print(e)
+
+    # sort
+    print('sort med_list in id_med by time')
+    for patid, med_list in id_med.items():
+        med_list_sorted = sorted(set(med_list), key=lambda x: x[0])
         id_med[patid] = med_list_sorted
 
     print('Total Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
@@ -365,21 +535,21 @@ def combine_2_id_med(id_med1, id_med2, output_file=''):
     print('In combine_2_id_med:',
           'len(id_med1)', len(id_med1), 'len(id_med2)', len(id_med2),
           'output_file:', output_file)
-    id_med = defaultdict(set)
+    id_med = defaultdict(list)
     for patid, med_list in id_med1.items():
-        id_med[patid] = set(med_list)
+        id_med[patid] = list(med_list)
 
     for patid, records in id_med2.items():
         if patid not in id_med:
-            id_med[patid] = set(records)
+            id_med[patid] = list(records)
         else:
-            id_med[patid].update(records)
+            id_med[patid].extend(records)
     print('Combined len(id_med):', len(id_med))
 
     # sort
     print('sort combined id_med by time')
     for patid, med_list in id_med.items():
-        med_list_sorted = sorted(med_list, key=lambda x: x[0])
+        med_list_sorted = sorted(set(med_list), key=lambda x: x[0])
         id_med[patid] = med_list_sorted
 
     if output_file:
@@ -405,16 +575,23 @@ if __name__ == '__main__':
         print('len(selected_patients):', len(selected_patients))
 
     print('args.dataset:', args.dataset)
-    print("step 1. prescribe")
-    id_med1 = read_prescribing(args.prescribe_file, args.prescribe_output, selected_patients)
-    print('read_prescribing done, len(id_med1):', len(id_med1))
 
-    print("step 2. med_admin")
-    id_med2 = read_med_admin(args.med_admin_file, args.med_admin_output, selected_patients)
-    print('read_med_admin done, len(id_med2):', len(id_med2))
+    # print("step 1. prescribe")
+    # id_med1 = read_prescribing(args.prescribe_file, args.prescribe_output, selected_patients)
+    # print('read_prescribing done, len(id_med1):', len(id_med1))
+    #
+    # print("step 2. med_admin")
+    # id_med2 = read_med_admin(args.med_admin_file, args.med_admin_output, selected_patients)
+    # print('read_med_admin done, len(id_med2):', len(id_med2))
 
-    print("step 3. combine both")
-    id_med = combine_2_id_med(id_med1, id_med2, args.output_file)
+    print("step 3. dispensing")
+    id_med3 = read_dispensing(args.dispensing_file, args.dispensing_output, selected_patients)
+    print('DISPENSE_DATE done, len(id_med2):', len(id_med3))
+
+    print("step 4. combine both")
+    id_med = combine_2_id_med(id_med1, id_med2,)
+    id_med = combine_2_id_med(id_med, id_med3, args.output_file)
+
     print('combine_2_id_med done, len(id_med):', len(id_med))
 
     print('Done! Time used:', time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)))
