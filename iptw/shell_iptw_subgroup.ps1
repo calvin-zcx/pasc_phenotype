ec2ef2542b161761339cdb28b5b4bad1eff0b3bc
Chengxi Zang <calvin-zcx@users.noreply.github.com>
2024-02-17 15:44:13 -0500

    revise death capture, and age for not risk patients, add subgroup

---
+++ iptw/shell_iptw_subgroup.ps1
@@ -0,0 +1,20 @@
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Cancer' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Cancer.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Chronic kidney disease' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Chronic_kidney_disease.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Chronic liver disease' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Chronic_liver_disease.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Chronic lung disease' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Chronic_lung_disease.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Cystic fibrosis' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Cystic_fibrosis.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Dementia or other neurological conditions' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Dementia_or_other_neurological_conditions.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Diabetes' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Diabetes.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Disabilities' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Disabilities.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Heart conditions' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Heart_conditions.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Hypertension' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Hypertension.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:HIV infection' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_HIV_infection.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Immunocompromised condition or weakened immune system' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Immunocompromised_condition_or_weakened_immune_system.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Mental health conditions' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Mental_health_conditions.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Overweight and obesity' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Overweight_and_obesity.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Pregnancy' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Pregnancy.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Sickle cell disease or thalassemia' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Sickle_cell_disease_or_thalassemia.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Smoking current' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Smoking_current.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Stroke or cerebrovascular disease' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Stroke_or_cerebrovascular_disease.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Substance use disorders' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Substance_use_disorders.txt
+python screen_paxlovid_iptw_pcornet.py  --cohorttype atrisk --severity 'PaxRisk:Tuberculosis' 2>&1 | tee  log_recover/screen_paxlovid_iptw_pcornet-atrisk-PaxRisk_Tuberculosis.txt
