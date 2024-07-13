a8174dce461875838c247cf9b27e04c8572ddc3f
chz4001 <chz4001@med.cornell.edu>
2024-07-13 03:26:30 +0000

    add columns ssri 'vilazodone' and preprocess build cohort

--- iptw/screen_ssri_iptw_pcornet_comb.py
+++ iptw/screen_ssri_iptw_pcornet_comb.py
@@ -447,8 +447,8 @@ if __name__ == "__main__":
 
     treadcol = 'ssri-treat-0-15-flag'
     print('exposre strategy', treadcol)
-    appendix = 'vs-snri-0-15-allmental'
-    df1 = df.loc[(df['ssri-treat-0-15-flag'] >= 1)
+    appendix = 'vs-snri-30-30-allmental'
+    df1 = df.loc[(df['ssri-treat--30-30-flag'] >= 1)
                  & (df['PaxRisk:Mental health conditions'] > 0)
                  & (df['snri-treat--180-180-flag'] == 0), :]
     df1['treated'] = 1
@@ -458,12 +458,12 @@ if __name__ == "__main__":
 
     print('check exposure strategy, n1, negative ratio, n0, if match on mental health, no-user definition')
     # df0 = df.loc[(df['ssri-treat--120-0-flag'] == 0) & (df['snri-treat--120-0-flag'] == 0), :]
-    df0 = df.loc[(df['snri-treat-0-15-flag'] >= 1)
+    df0 = df.loc[(df['snri-treat--30-30-flag'] >= 1)
                  & (df['PaxRisk:Mental health conditions'] > 0)
                  & (df['ssri-treat--180-180-flag'] == 0), :]
 
     print('n0', len(df0))
-    df0 = df0.sample(n=int(args.negative_ratio * n1), replace=False, random_state=args.random_seed)
+    df0 = df0.sample(n=min(int(args.negative_ratio * n1), len(df0)), replace=False, random_state=args.random_seed)
     print('after sample, n0', len(df0))
     df0['treated'] = 0
     df0['SSRI'] = 0
