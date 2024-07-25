971ebd04f64bdbf2f18b24de0bc6643fd9cb96dc
chz4001 <chz4001@med.cornell.edu>
2024-07-25 19:54:16 +0000

    ssri shell

---
+++ iptw/ssri_shell_1.ps1
@@ -0,0 +1,6 @@
+python screen_ssri_iptw_pcornet.py  --exptype ssri-base-180-0  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-ssri-base-180-0.txt
+python screen_ssri_iptw_pcornet.py  --exptype ssri-acute0-7  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-ssri-acute0-7.txt
+python screen_ssri_iptw_pcornet.py  --exptype snri-base-180-0  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-snri-base-180-0.txt
+python screen_ssri_iptw_pcornet.py  --exptype snri-acute0-7  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-snri-acute0-7.txt
+python screen_ssri_iptw_pcornet.py  --exptype ssriVSsnri-base-180-0  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-ssriVSsnri-base-180-0.txt
+python screen_ssri_iptw_pcornet.py  --exptype ssriVSsnri-acute0-7  2>&1 | tee  log_ssri/screen_ssri_iptw_pcornet-ssriVSsnri-acute0-7.txt
